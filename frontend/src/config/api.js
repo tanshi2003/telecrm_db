2809698d072d88b5298aa5cd18e5f41867398b81
Tanshi Khandelwal <tanshikhandelwal03@gmail.com>
2025-07-22 17:15:56 +0530

    Fixes!!

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: BASE_URL/api,
     headers: {
         'Content-Type': 'application/json'
     }
